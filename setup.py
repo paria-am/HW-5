3485bef129b54368801e061160545fe9bb31acfb
paria <pariya.amiri92@gmail.com>
2022-12-04 18:48:04 +0100

    Make installable

--- setup.py
+++ setup.py
@@ -1,11 +1,2 @@
 from setuptools import setup
-from setuptools import find_packages
-
-
-setup(
-	name = "snowflake",
-	version = "1.0.0",
-	description = "This package contains some sample codes",
-	author = "Paria Amiri",
-	packages = find_packages(),
-)
+setup()
