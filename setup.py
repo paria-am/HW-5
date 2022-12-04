3f2f4b2b3346b3808e39b7318ae5b50ee8983d46
paria <pariya.amiri92@gmail.com>
2022-12-04 20:37:04 +0100

    Make installable

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
 from setuptools import setup
-setup()
+from setuptools import find_packages
+
+
+setup(
+	name = "snowflake",
+	version = "1.0.0",
+	description = "This package contains some sample codes",
+	author = "Paria Amiri",
+	packages = find_packages(),
+	entry_points = {
+		"console_scripts":[
+			"snowflake-cli = snowflake.let_it_snow:main"
+		],
+)
